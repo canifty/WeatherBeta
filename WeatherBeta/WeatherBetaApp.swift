8253b39c6ead6ab5d33d4a7ff0759387f7a02c90
canifty <can-dindar@outlook.com>
2024-07-15 21:59:37 -0400

    Initial Commit

---
+++ WeatherBeta/WeatherBetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherBetaApp.swift
+//  WeatherBeta
+//
+//  Created by Can Dindar on 2024-07-15.
+//
+
+import SwiftUI
+
+@main
+struct WeatherBetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
